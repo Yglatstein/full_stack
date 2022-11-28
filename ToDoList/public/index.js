90c0551ff0e497736fb923f472531f1ea4417603
Yglatstein <41186562+Yglatstein@users.noreply.github.com>
2022-11-28 20:23:36 +0200

    TDL
    TDL project

---
+++ ToDoList/public/index.js
@@ -0,0 +1,37 @@
+"use strict";
+let addTaskButton = document.getElementsByClassName("submit")[0];
+let tasksDiv = document.getElementsByClassName("toDoList")[0];
+let tasksList = document.getElementsByClassName("taskRaw");
+let taskNo = 0;
+addTaskButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    let taskText = document.getElementById('task');
+    createNewTask(taskText.value);
+    taskText.value = "";
+});
+function delTask() {
+    let toRmv;
+    for (let i = 0; i < tasksList.length; i++) {
+        console.log(tasksList[i]);
+        if (tasksList[i].getElementsByTagName("button")[0] == this) {
+            toRmv = i;
+            break;
+        }
+    }
+    tasksList[toRmv].remove();
+}
+function createNewTask(text) {
+    let taskRaw = document.createElement("div");
+    taskRaw.classList.add("taskRaw");
+    let taskPar = document.createElement("p");
+    taskPar.textContent = text;
+    console.log(text);
+    taskPar.classList.add("taskText");
+    let taskDel = document.createElement("button");
+    taskDel.classList.add("delTaskBtn");
+    taskRaw.appendChild(taskPar);
+    taskRaw.appendChild(taskDel);
+    taskRaw.classList.add(taskNo.toString());
+    tasksDiv.appendChild(taskRaw);
+    taskRaw.getElementsByTagName("button")[0].addEventListener("click", delTask);
+}
